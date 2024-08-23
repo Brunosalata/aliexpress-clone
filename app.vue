634d074c8bb666b89888b029be7a6848af599aae
Bruno Salata <brunoslima@gmail.com>
2024-08-23 14:59:09 -0300

    MainLayout, components Footer and Loading, store

--- app.vue
+++ app.vue
@@ -1,6 +1,4 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <div class="fixed z-[-1] bg-[#f2f2f2] w-full h-[100vh]"/>
+  <NuxtPage />
 </template>
---
+++ components/Footer.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="FooterMain" class="w-full bg-[#e8e8e8] mt-10 mb-2">
+        <div class="max-w-[1200px] mx-auto px-4">
+            <div class="md:flex gap-4 justify-between w-full">
+                <div class="md:w-[50%] pt-4">
+                    <div class="text-lg font-semibold text-[#282828]">Help</div>
+                    <div class="text-sm text-[#999999]">Help Center, Disputes & Reports, Buyer Protection</div>
+                </div>
+                <div class="md:w-[50%] pt-4">
+                    <div class="text-lg font-semibold text-[#282828]">Aliexpress Multi-Language Sites</div>
+                    <div class="text-sm text-[#999999]">Portuguese, English, Spanish</div>
+                </div>
+            </div>
+
+            <div class="md:flex gap-4 justify-between w-full">
+                <div class="md:w-[50%] pt-4">
+                    <div class="text-lg font-semibold text-[#282828]">Browser by Category</div>
+                    <div class="text-sm text-[#999999]">All Popular, Promotion, Low Price, Great Values, Reviews</div>
+                </div>
+                <div class="md:w-[50%] pt-4">
+                    <div class="text-lg font-semibold text-[#282828]">Alibaba Group</div>
+                    <div class="text-sm text-[#999999]">Alibaba Group Website, Aliexpress, Alimama, Alipay, Fliggy, Alibaba Cloud</div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div id="FooterSub" class="w-full bg-[#333333] text-sm text-gray-400">
+<div class="max-w-[1200px] mx-auto p-4">
+Intellectual Property Protection - Privacy Policy - Sitemap - Terms of Use ©️ 2010-2024 AliExpress.com. Todos os direitos reservados.
+</div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
